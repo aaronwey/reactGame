e91cf4b978c5b7e2adfb6c40ca501cbd74a81bb8
aaronwey <aaronwey@gmail.com>
2017-12-21 13:18:29 -0700

    last things to add: alert when lose, and tidy up style

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import Section from './components/Section';
 import pics from './pics.json';
 
 class App extends React.Component {
-  // Setting the initial state of the Counter component
   
 	state = {
 	    count: 0,
-	    pics,
-	    clicked: []
+	    pics, 
+	    clicked: [],
+	    highScore: 0
 	};
 	
-
 	handleIncrement = id => {
 
 		let clicked = this.state.clicked.includes(id) ? (this.state.clicked.splice(0), clicked=this.state.clicked) : this.state.clicked.concat(id);
@@ -21,18 +20,18 @@ class App extends React.Component {
 		console.log(this.state.clicked);
 		console.log(clicked);
 		this.setState({ count: clicked.length });
+		clicked.length > this.state.highScore ? this.setState({ highScore: clicked.length }) : this.setState({ highScore: this.state.highScore })
 		const shuffled = pics.map((a) => [Math.random(),a]).sort((a,b) => a[0]-b[0]).map((a) => a[1]);
 		this.setState ({ pics: shuffled })
-	    
+	   
 	};
-
-
 
 	render(){
 		return(
 			<div>
 				<Navbar 
 					count={this.state.count}
+					highScore={this.state.highScore}
 					handleIncrement={this.handleIncrement}
 				/>
 				<Header />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,21 +5,10 @@ const Navbar = props => (
 	  <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
 	    <span className="navbar-toggler-icon"></span>
 	  </button>
-	  <a className="navbar-brand" href="#">Navbar w/ text</a>
+	  
 	  <div className="collapse navbar-collapse" id="navbarText">
-	    <ul className="navbar-nav mr-auto">
-	      <li className="nav-item active">
-	        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-	      </li>
-	      <li className="nav-item">
-	        <a className="nav-link" href="#">Features</a>
-	      </li>
-	      <li className="nav-item">
-	        <a className="nav-link" href="#">Pricing</a>
-	      </li>
-	    </ul>
 	    <span className="navbar-text">
-	      Score: {props.count}
+	      Score: {props.count} High Score: {props.highScore}
 	    </span>
 	  </div>
 	</nav>
